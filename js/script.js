1a240f14be39a909fadc7a95edda0e5de30d9b0a
Oskar Siry <oskarsiry99@gmail.com>
2023-02-21 11:59:32 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Cześć!");
+
+let przycisk = document.querySelector(".przycisk");
+let zdjecie = document.querySelector(".zdjecie");
+
+przycisk.addEventListener("click", () => {
+    zdjecie.remove();
+});
